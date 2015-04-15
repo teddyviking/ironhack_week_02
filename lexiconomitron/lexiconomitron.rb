75224f70aa5727eba6cd3a4d8819ff074d14046d
Rafael Tejado <rtejado90@gmail.com>
2015-04-15 18:10:36 +0200

    Add method to eat downcase T letters

--- lexiconomitron/lexiconomitron.rb
+++ lexiconomitron/lexiconomitron.rb
@@ -1,1 +1,7 @@
 require "pry"
+
+class Lexiconomitron
+	def eat_letters(word)
+		word.gsub('t', "")
+	end
+end
--- lexiconomitron/spec/lexiconomitron_spec.rb
+++ lexiconomitron/spec/lexiconomitron_spec.rb
@@ -1,1 +1,13 @@
 require_relative('../lexiconomitron')
+
+RSpec.describe "Lexiconomitron" do
+	it "eats downcase T letters of a word" do
+		word = "tomato"
+		expect(Lexiconomitron.new.eat_letters(word)).to eq("omao")
+	end
+	it "eats upcase and downcase T letters of a word" do
+		word = "Tomato"
+		expect(Lexiconomitron.new.eat_letters(word)).to eq("omao")
+	end
+
+end
