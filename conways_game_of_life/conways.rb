9bd5fc0637473caec24eb1cead28c9058fa72b11
Rafael Tejado <rtejado90@gmail.com>
2015-04-16 14:55:21 +0200

    Refactor to improve independency between classes

--- conways_game_of_life/conways.rb
+++ conways_game_of_life/conways.rb
@@ -9,15 +9,27 @@ class Cell
 	end
 
 	def evolve
+		change_state if cell_evolver.evolve?(@state, @neighbours)
+
 		if @state == :alive
 			@state = :dead if @neighbours < 2 || @neighbours > 3
 		else
 			@state = :alive if @neighbours == 3
 		end		
 	end
+
+	def change_state
+		@state == :alive ? @state = :dead : @state = :alive
+	end
 end
 
 
 class CellEvolver
-
+	def evolve?(state, neighbours)
+		if @state == :alive
+			@neighbours < 2 || @neighbours > 3
+		elsif @state == :dead
+			@neighbours == 3
+		end	
+	end
 end
--- conways_game_of_life/spec/conways_spec.rb
+++ conways_game_of_life/spec/conways_spec.rb
@@ -1,28 +1,29 @@
 require_relative('../conways')
 
 RSpec.describe "Cell Evolver" do
+	let (:cell_evolver) {CellEvolver.new}
 	it "kills a cell that is alone" do
-		alive_cell = Cell.new(:alive, 0, CellEvolver.new)
+		alive_cell = Cell.new(:alive, 0, cell_evolver)
 		alive_cell.evolve
 		expect(alive_cell.state).to eq(:dead)
 	end
 	it "keeps a cell alive that has two neighbours" do
-		alive_cell = Cell.new(:alive, 2, CellEvolver.new)
+		alive_cell = Cell.new(:alive, 2, cell_evolver)
 		alive_cell.evolve
 		expect(alive_cell.state).to eq(:alive)
 	end
 	it "kills a cell that has four neighbours" do
-		alive_cell = Cell.new(:alive, 4, CellEvolver.new)
+		alive_cell = Cell.new(:alive, 4, cell_evolver)
 		alive_cell.evolve
 		expect(alive_cell.state).to eq(:dead)
 	end
 	it "resurrects a dead cell that has 3 neighbours" do
-		dead_cell = Cell.new(:dead, 3, CellEvolver.new)
+		dead_cell = Cell.new(:dead, 3, cell_evolver)
 		dead_cell.evolve
 		expect(dead_cell.state).to eq(:alive)
 	end
 	it "does not resurrect a cell with 2 neighbours" do
-		dead_cell = Cell.new(:dead, 2, CellEvolver.new)
+		dead_cell = Cell.new(:dead, 2, cell_evolver)
 		dead_cell.evolve
 		expect(dead_cell.state).to eq(:dead)
 	end
